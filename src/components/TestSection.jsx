804867a098004284a8ae45239ba0fb69cb5b553b
Ebron Sasack <ebronsasack7@gmail.com>
2025-06-14 21:11:46 +0530

    Sasack

--- src/components/TestSection.jsx
+++ src/components/TestSection.jsx
@@ -1,6 +1,3 @@
-import User1 from '../assets/profile2.jpg'
-import User2 from '../assets/profile3.jpg'
-
 
 
 export const TestSection = ()=>{
@@ -10,7 +7,7 @@ export const TestSection = ()=>{
                 <h2 className="text-4xl font-bold">What Developers Say</h2>
                 <div className="mt-12 grid grid-cols-1 md:grid-cols-2 gap-12">
                     <div className="bg-gray-900 p-8 rounded-lg shadow-lg flex items-center gap-6">
-                        <img src={User1} alt="User image" className='w-[52px] h-[52px] rounded-full border-2 border-purple-400 object-cover' />
+                        <img src="profile/Profile2.jpg" alt="User image" className='w-[52px] h-[52px] rounded-full border-2 border-purple-400 object-cover' />
                         <div className='text-left'>
                             <p className='text-gray-400 text-lg '>"LaunchFlow made our deployment effortless and fast"</p>
                             <h4 className='text-lg font-semibold mt-4'>Nancy Wheelers</h4>
@@ -18,7 +15,7 @@ export const TestSection = ()=>{
                         </div>
                     </div>
                     <div className="bg-gray-900 p-8 rounded-lg shadow-lg flex items-center gap-6">
-                        <img src={User2} alt="User image" className='w-[52px] h-[52px] rounded-full border-2 border-blue-400 object-cover' />
+                        <img src="profile/Profile3.jpg" alt="User image" className='w-[52px] h-[52px] rounded-full border-2 border-blue-400 object-cover' />
                         <div className='text-left'>
                             <p className='text-gray-400 text-lg '>"A game changer for frontend developers"</p>
                             <h4 className='text-lg font-semibold mt-4'>John Wick</h4>
